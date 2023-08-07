95ac86db6d39a769591b2b1fbed4a4ac4c4d8400
TylerTran4201 <trandongtri.work@gmail.com>
2023-08-07 18:10:18 +0700

    first commit

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,26 @@
+using API.Data;
+using API.Helpers;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+            IConfiguration config)
+        {
+            services.AddCors();
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+            services.AddScoped<IPhotoService, PhotoService>();
+            services.AddScoped<LogUserActivity>();
+            services.AddScoped<ILikesRepository, LikesRepository>();
+
+            return services;
+        }
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,70 @@
+using API.Data;
+using API.Extensions;
+using API.Middleware;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
+
+var connString = "";
+if (builder.Environment.IsDevelopment()) 
+    connString = builder.Configuration.GetConnectionString("DefaultConnection");
+else 
+{
+// Use connection string provided at runtime by FlyIO.
+        var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+        // Parse connection URL to connection string for Npgsql
+        connUrl = connUrl.Replace("postgres://", string.Empty);
+        var pgUserPass = connUrl.Split("@")[0];
+        var pgHostPortDb = connUrl.Split("@")[1];
+        var pgHostPort = pgHostPortDb.Split("/")[0];
+        var pgDb = pgHostPortDb.Split("/")[1];
+        var pgUser = pgUserPass.Split(":")[0];
+        var pgPass = pgUserPass.Split(":")[1];
+        var pgHost = pgHostPort.Split(":")[0];
+        var pgPort = pgHostPort.Split(":")[1];
+
+        connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+}
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseNpgsql(connString);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("https://localhost:4200"));
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseDefaultFiles();
+app.UseStaticFiles ();
+
+app.MapControllers();
+app.MapFallbackToController("Index","Fallback");
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try{
+    var context =services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+}catch(Exception ex){
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error occureed during migration");
+}
+
+app.Run();
+
+
